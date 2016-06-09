3218f692d3d99a1a1dcb75f6c58bda89b44db39a
Pietro <donapieppo@yahoo.it>
2016-06-09 15:56:55 +0200

    example configuration

---
+++ doc/configuration.rb
@@ -0,0 +1,19 @@
+module DmUniboCommon
+  HOST             = 'www.example.it'
+  HOST_TEST        = 'tester.example.it'
+  SMTP_ADDRESS     = 'mailhost.example.it'
+  SMTP_DOMAIN      = 'example.it'
+  SUPPORT_MAIL     = 'supportoweb@example.it'
+
+  LOGOUT_LINK      = 'https://exmple.it/adfs/ls/?wa=wsignout1.0'
+  LOGOUT_LINK_TEST = 'https://exmple2.it/adfs/ls/?wa=wsignout1.0'
+  LOGIN_ICON       = 'dm_unibo_common/ssologo18x18.png'
+  LOGOUT_ICON      = 'dm_unibo_common/ssologo18x18.png'
+
+  LOGO_PAGE        = 'http://www.unibo.it/'
+  LOGO_IMAGE       = 'dm_unibo_common/unibo.png'
+
+  CHIEF            = 'most.important@example.it'
+  CESIA_UPN        = ['admin.user1@example.it', 'admin.user2@example.it']
+end
+
