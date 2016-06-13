1836963f5fbb0883f1f3d3a42cd313ae2d9b32fd
Pietro <donapieppo@yahoo.it>
2016-06-13 18:06:18 +0200

    comments

--- doc/configuration.rb
+++ doc/configuration.rb
@@ -1,15 +1,18 @@
 module DmUniboCommon
+  # used in mail 
   HOST             = 'www.example.it'
   HOST_TEST        = 'tester.example.it'
+  # see config/initializers/setup_mail.rb and footer
   SMTP_ADDRESS     = 'mailhost.example.it'
   SMTP_DOMAIN      = 'example.it'
   SUPPORT_MAIL     = 'supportoweb@example.it'
 
-  LOGOUT_LINK      = 'https://exmple.it/adfs/ls/?wa=wsignout1.0'
-  LOGOUT_LINK_TEST = 'https://exmple2.it/adfs/ls/?wa=wsignout1.0'
   LOGIN_ICON       = 'dm_unibo_common/ssologo18x18.png'
   LOGOUT_ICON      = 'dm_unibo_common/ssologo18x18.png'
+  LOGOUT_LINK      = 'https://exemple.it/adfs/ls/?wa=wsignout1.0'
+  LOGOUT_LINK_TEST = 'https://exemple2.it/adfs/ls/?wa=wsignout1.0'
 
+  # logo od bootstrap header
   LOGO_PAGE        = 'http://www.unibo.it/'
   LOGO_IMAGE       = 'dm_unibo_common/unibo.png'
 
