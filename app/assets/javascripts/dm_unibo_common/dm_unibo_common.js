e8253faa2a30a762d13338ab1c2a0de2acd1fb91
Pietro <donapieppo@yahoo.it>
2018-02-26 11:25:45 +0100

    new modal link js

--- app/assets/javascripts/dm_unibo_common/dm_unibo_common.js
+++ app/assets/javascripts/dm_unibo_common/dm_unibo_common.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
   $("input:submit").button();
   $("a.button").button();
+
   //$('input.date_picker').datepicker({
   //  language: 'it', 
   //  format: "dd/mm/yyyy"
   //});
 
+  $('.modal-link').click(function(event){
+    event.preventDefault();
+    var url = $(this).attr('href');
+    var separator = url.indexOf('?') > -1 ? '&' : '?';
+    $('#main-modal .modal-body').load(url + separator + "modal=yyy");
+    $('#main-modal').modal('show');
+  });
 });
 
 
