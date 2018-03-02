c5ff0fd15210879dcf2a0ced73520a90d333f350
Pietro <donapieppo@yahoo.it>
2018-03-02 12:45:35 +0100

    modal_link_to

--- app/helpers/dm_unibo_link_helper.rb
+++ app/helpers/dm_unibo_link_helper.rb
@@ -32,6 +32,10 @@ module DmUniboLinkHelper
   end
 
   # REMOTE
+  def modal_link_to(name, url)
+    link_to name, url, class: 'modal-link', modal: true
+  end
+
   def link_to_modal_edit(url)
     link_to icon('edit'), url, title: 'Inserisci/modifica dati', data: { toggle: "modal", target: "#main-modal" }
   end
