06ac3609d88aa6c2351e79a4b3be60c06eb4b43a
Pietro <donapieppo@yahoo.it>
2018-07-27 12:36:47 +0200

    add class and not substitute

--- app/inputs/date_picker_input.rb
+++ app/inputs/date_picker_input.rb
@@ -3,14 +3,14 @@ class DatePickerInput < SimpleForm::Inputs::Base
   def input(wrapper_options)
     passed_date = object ? object.send(attribute_name) : Date.today
     val = I18n.localize(passed_date || Date.today, format: :date_picker)
+
+    merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
     @builder.text_field(attribute_name, 
                         input_html_options.merge(size:   10, 
                                                  width:  20,
                                                  value:  val, 
-                                                 class:  'date form-control date_picker', 
+                                                 class:  merged_input_options[:class] + ['date date_picker'], 
                                                  format: :date_picker))
-    # @builder.text_field(attribute_name, input_html_options.merge(:value => I18n.localize(object.date || Time.now, :format => :form)))
-    # @builder.text_field(attribute_name, input_html_options)
   end
 end
 
