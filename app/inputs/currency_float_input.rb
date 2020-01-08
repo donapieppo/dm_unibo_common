0b8e9aef9e24ed45c413cbfa148d75a82f170f25
Pietro <donapieppo@yahoo.it>
2020-01-08 11:42:05 +0100

    for new bootstrap. Notice input-group and is-invalid

--- app/inputs/currency_float_input.rb
+++ app/inputs/currency_float_input.rb
@@ -1,8 +1,22 @@
+# @builder.text_field(attribute_name, merged_input_html_options)  ->
+# "<input class="form-control ..." aria-invalid=\"true\" type=\"text\" value=\"1000.0\" name=\"repayment[payment]\" id=\"repayment_payment\" />"
 class CurrencyFloatInput < SimpleForm::Inputs::Base
   def input(wrapper_options)
-    input_html_options[:class] << 'form-control-inline numeric integer'
-    @builder.text_field(attribute_name, input_html_options) + 
-    template.content_tag(:span, " <strong>&euro;</strong>".html_safe)
-    #template.content_tag(:span, ",00 &euro;".html_safe, class: "input-group-addon")
+    currency = (options.delete(:currency) || '€').html_safe
+
+    # {:class=>[:currency_float2, :optional], :required=>false, :"aria-required"=>nil, :"aria-invalid"=>true}
+    input_html_options[:class] << 'numeric integer'
+    merged_input_html_options = merge_wrapper_options(input_html_options, wrapper_options)
+
+    input_group_class = 'input-group' # to have input-group-prepend
+    input_group_class << ' is-invalid' if @builder.object.errors[attribute_name] # or .invalid-feedback is not displayed
+
+    template.content_tag(:div, class: input_group_class) do 
+      template.content_tag(:div, class: "input-group-prepend") do
+        template.content_tag(:div, currency, class: "input-group-text") 
+      end +
+      @builder.text_field(attribute_name, merged_input_html_options) 
+    end
   end
 end
+
