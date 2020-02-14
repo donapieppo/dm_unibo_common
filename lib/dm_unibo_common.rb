b9a14ffb053a0156b760d0529038adf1355cd4dc
Pietro <donapieppo@yahoo.it>
2020-02-14 14:53:32 +0100

    clean

--- lib/dm_unibo_common.rb
+++ lib/dm_unibo_common.rb
@@ -2,9 +2,6 @@ require "dm_unibo_common/engine"
 
 # rails
 require 'mysql2'
-require 'jquery-rails'
-require 'sassc-rails'
-require 'bootstrap'
 
 # auth
 require 'omniauth'
@@ -13,17 +10,17 @@ require 'omniauth-google-oauth2'
 require 'pretender'
 require 'pundit'
 
-require 'font-awesome-sass'
-
 # self
 require 'dm_unibo_common/errors'
 require 'dm_unibo_common/development_mail_interceptor'
 require 'dm_unibo_common/user'
 require 'dm_unibo_common/organization'
 require 'dm_unibo_common/authorization'
-require 'dm_unibo_common/permission'
+# require 'dm_unibo_common/permission'
 require 'dm_unibo_common/user_upn_methods'
 require 'dm_unibo_common/controllers/helpers'
+require 'dm_unibo_common/policies/application_policy'
+require 'dm_unibo_common/policies/organization_policy'
 
 module DmUniboCommon
 end
