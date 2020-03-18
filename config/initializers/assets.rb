d9f99e8992cb7d32cf2c411b8571d5b1c8da51c8
Pietro <donapieppo@yahoo.it>
2020-03-18 15:37:24 +0100

    assets

--- config/initializers/assets.rb
+++
@@ -1,5 +0,0 @@
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
-
-Rails.application.config.assets.precompile += %w( dm_unibo_common/unibo.png dm_unibo_common/ssologo18x18.png dm_unibo_common/cc.png )
