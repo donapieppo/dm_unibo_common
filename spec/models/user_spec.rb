1092cc662ce5bfa329d49875058f957f1643f845
Pietro <donapieppo@yahoo.it>
2020-04-27 13:06:02 +0200

    user_spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject { FactoryBot.create(:user) }
+  let(:org1) { FactoryBot.create(:organization) }
+  let(:org2) { FactoryBot.create(:organization) }
+
+  it ".authorization is not" do
+    expect(subject.authorization).not_to be
+  end
+
+  it ".update_authorization_by_ip updates .authorization to DmUniboCommon::Authorization" do
+    subject.update_authorization_by_ip('1.1.1.1')
+    expect(subject.authorization).to be_a(DmUniboCommon::Authorization)
+  end
+end
+
