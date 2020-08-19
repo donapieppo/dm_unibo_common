da65680f638132c1b3c77594d76447939ce3e633
Pietro <donapieppo@yahoo.it>
2020-08-19 15:10:14 +0200

    moved here common controllers preamble

--- app/controllers/dm_unibo_common/application_controller.rb
+++ app/controllers/dm_unibo_common/application_controller.rb
@@ -1,5 +1,17 @@
 module DmUniboCommon
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+    include Pundit
+
+    include DmUniboCommon::Controllers::Helpers
+
+    impersonates :user, method: :current_user, with: ->(id) { DmUniboCommon::CurrentUser.find(id) }
+
+    before_action :set_current_organization, :set_current_user, :update_authorization, :log_current_user
+
+    def default_url_options(_options={})
+      _options[:__org__] = current_organization ? current_organization.code : nil
+      _options
+    end
   end
 end
