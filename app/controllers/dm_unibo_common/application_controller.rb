9ca288057540a0d4f47243015b772278fc105e12
Pietro <donapieppo@yahoo.it>
2020-08-21 17:00:22 +0200

    possible refactor with DmUniboCommon::CurrentUser module instead of subclass. maybe ok with current_user.extend(DmUniboCommon::CurrentUser) in update_authorization

--- app/controllers/dm_unibo_common/application_controller.rb
+++ app/controllers/dm_unibo_common/application_controller.rb
@@ -5,9 +5,10 @@ module DmUniboCommon
 
     include DmUniboCommon::Controllers::Helpers
 
-    impersonates :user, method: :current_user, with: ->(id) { DmUniboCommon::CurrentUser.find(id) }
+    #impersonates :user, method: :current_user, with: ->(id) { DmUniboCommon::CurrentUser.find(id) }
+    impersonates :user, method: :current_user, with: ->(id) { ::User.find(id) }
 
-    before_action :set_current_user, :set_current_organization, :update_authorization, :log_current_user
+    before_action :set_current_user, :update_authorization, :set_current_organization, :log_current_user
 
     def default_url_options(_options={})
       _options[:__org__] = current_organization ? current_organization.code : nil
--- app/models/dm_unibo_common/current_user.rb
+++ app/models/dm_unibo_common/current_user.rb
@@ -1,11 +1,20 @@
-module DmUniboCommon
-  class CurrentUser < ::User
-    self.inheritance_column = :_type_disabled
-    # DmUniboCommon::Authorization (access levels to organizations)
-    attr_reader :authorization 
+module DmUniboCommon::CurrentUser
+  def authorization
+    @_authorization
+  end
 
-    def update_authorization_by_ip(ip)
-      @authorization = DmUniboCommon::Authorization.new(ip, self)
-    end
+  def update_authorization_by_ip(ip)
+    @_authorization = DmUniboCommon::Authorization.new(ip, self)
   end
 end
+# module DmUniboCommon
+#   class CurrentUser < ::User
+#     self.inheritance_column = :_type_disabled
+#     # DmUniboCommon::Authorization (access levels to organizations)
+#     attr_reader :authorization 
+
+#     def update_authorization_by_ip(ip)
+#       @authorization = DmUniboCommon::Authorization.new(ip, self)
+#     end
+#   end
+# end
