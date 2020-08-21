e74e3680e0a1370fe4fa0fa7235a7ffdf06d903a
Pietro <donapieppo@yahoo.it>
2020-08-21 13:26:15 +0200

    4 hours bug!!!!

--- app/controllers/dm_unibo_common/application_controller.rb
+++ app/controllers/dm_unibo_common/application_controller.rb
@@ -7,7 +7,7 @@ module DmUniboCommon
 
     impersonates :user, method: :current_user, with: ->(id) { DmUniboCommon::CurrentUser.find(id) }
 
-    before_action :set_current_organization, :set_current_user, :update_authorization, :log_current_user
+    before_action :set_current_user, :set_current_organization, :update_authorization, :log_current_user
 
     def default_url_options(_options={})
       _options[:__org__] = current_organization ? current_organization.code : nil
