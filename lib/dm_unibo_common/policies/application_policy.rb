3af34839fdde3b072a565769e52f17abc2a35a53
Pietro <donapieppo@yahoo.it>
2020-08-26 16:20:42 +0200

    policy

--- lib/dm_unibo_common/policies/application_policy.rb
+++ lib/dm_unibo_common/policies/application_policy.rb
@@ -1,63 +1,63 @@
 module DmUniboCommon
-class ApplicationPolicy
-  attr_reader :user, :record
+  class ApplicationPolicy
+    attr_reader :user, :record
 
-  def initialize(user, record)
-    @user = user
-    @record = record
-  end
-
-  def owner?
-    @user && @record.user_id == @user.id
-  end
-
-  def record_organization_manager?
-    @user && @user.authorization.can_manage?(@record.organization_id)
-  end
-
-  def owner_or_record_organization_manager?
-    record_organization_manager? || owner?
-  end
-
-  def index?
-    false
-  end
-
-  def show?
-    false
-  end
-
-  def create?
-    false
-  end
-
-  def new?
-    create?
-  end
-
-  def update?
-    false
-  end
-
-  def edit?
-    update?
-  end
-
-  def destroy?
-    false
-  end
-
-  class Scope
-    attr_reader :user, :scope
-
-    def initialize(user, scope)
+    def initialize(user, record)
       @user = user
-      @scope = scope
+      @record = record
     end
 
-    def resolve
-      scope.all
+    def owner?
+      @user && @record.user_id == @user.id
+    end
+
+    def record_organization_manager?
+      @user && @user.authorization.can_manage?(@record.organization_id)
+    end
+
+    def owner_or_record_organization_manager?
+      record_organization_manager? || owner?
+    end
+
+    def index?
+      false
+    end
+
+    def show?
+      false
+    end
+
+    def create?
+      false
+    end
+
+    def new?
+      create?
+    end
+
+    def update?
+      false
+    end
+
+    def edit?
+      update?
+    end
+
+    def destroy?
+      false
+    end
+
+    class Scope
+      attr_reader :user, :scope
+
+      def initialize(user, scope)
+        @user = user
+        @scope = scope
+      end
+
+      def resolve
+        scope.all
+      end
     end
   end
-end
 end
