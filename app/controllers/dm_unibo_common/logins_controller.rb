ec9fed98fb68f88513f8d67c1c973f6df43c3dc7
Pietro <donapieppo@yahoo.it>
2020-10-07 15:19:39 +0200

    &&

--- app/controllers/dm_unibo_common/logins_controller.rb
+++ app/controllers/dm_unibo_common/logins_controller.rb
@@ -41,7 +41,7 @@ class LoginsController < ::ApplicationController
     log_unibo_omniauth
     parse_unibo_omniauth
 
-    if Rails.configuration.dm_unibo_common[:no_students] and @email !~ /@unibo.it$/
+    if Rails.configuration.dm_unibo_common[:no_students] && @email !~ /@unibo.it$/
       logger.info "Students are not allowed: #{@email} user not allowed."
       redirect_to no_access_path and return
     else
