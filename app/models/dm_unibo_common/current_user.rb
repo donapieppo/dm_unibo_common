2460b44e2698dcf990632f268d0f74d22ae02bbc
Pietro <donapieppo@yahoo.it>
2020-10-14 16:36:03 +0200

    current_user current_organization update_authorization_by_ip

--- app/models/dm_unibo_common/current_user.rb
+++ app/models/dm_unibo_common/current_user.rb
@@ -1,14 +1,23 @@
 # see dm_unibo_common/lib/dm_unibo_common/controllers/helpers.rb for
-# update_authorization where current_user.extend(DmUniboCommon::CurrentUser) && current_user.update_authorization_by_ip(request.remote_ip)
+# update_authorization which does
+# current_user.extend(DmUniboCommon::CurrentUser) && current_user.update_authorization_by_ip(request.remote_ip)
 # in app/controlers/application_controller.rb
 # before_action :set_current_user, :update_authorization
 module DmUniboCommon::CurrentUser
+  def update_authorization_by_ip(ip)
+    @_authorization = Authorization.new(ip, self)
+  end
+
+  def current_organization=(o)
+    @_current_organization = o
+  end
+
   def authorization
     @_authorization
   end
 
-  def update_authorization_by_ip(ip)
-    @_authorization = DmUniboCommon::Authorization.new(ip, self)
+  def current_organization
+    @_current_organization
   end
 
   def has_some_authorization?
