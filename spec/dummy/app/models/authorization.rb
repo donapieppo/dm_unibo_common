23d22fa5dfa47ecfdeb9207020e065894d8961a3
Pietro <donapieppo@yahoo.it>
2020-10-15 09:23:27 +0200

    configure_authlevels

--- spec/dummy/app/models/authorization.rb
+++ spec/dummy/app/models/authorization.rb
@@ -1,14 +1,5 @@
 class Authorization
   include DmUniboCommon::Authorization
-
-  _authlevels = { 
-    read:   1, 
-    manage: 2, 
-    pippo: 3
-  }
-
-  configure_authlevels(_authlevels)
-  DmUniboCommon::OrganizationPolicy.configure_authlevels(_authlevels)
-
+  configure_authlevels
 end
 
