655b048507fb6ad9e79bae03c7d3ac9ef7acafaf
Pietro <donapieppo@yahoo.it>
2020-10-15 09:25:04 +0200

    fixme

--- spec/models/permission_spec.rb
+++ spec/models/permission_spec.rb
@@ -1,16 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe DmUniboCommon::Permission, type: :model do
-  let (:read_permission)  { FactoryBot.create(:permission, authlevel: DmUniboCommon::Authorization::TO_READ) }
-  let (:write_permission) { FactoryBot.create(:permission, authlevel: DmUniboCommon::Authorization::TO_WRITE) }
+  let (:authlevels) { Authorization.all_authlevels }
+  let (:read_permission)  { FactoryBot.create(:permission, authlevel: 1) }
+  let (:write_permission) { FactoryBot.create(:permission, authlevel: 2) }
+  let (:pippo_permission) { FactoryBot.create(:permission, authlevel: 3) }
 
-  # NO 
-  # depends on session/ip/astral week
-  # it "read_permission means user.can_read?(organization) to be true" do
-  
-  #it "read_permission means user.can_read?(organization) to be true" do
-  #  expect(read_permission.user.can_read?(read_permission.organization)).to be(true)
-  #end
+  it "read_permission means user.can_read?(organization) to be true" do
+    # expect(read_permission.user.can_read?(read_permission.organization)).to be(true)
+  end
 
   #it "read_permission means user.can_manage?(organization) to be false" do
   #  expect(read_permission.user.can_manage?(read_permission.organization)).to be(true)
