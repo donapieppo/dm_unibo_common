5b97bfa89ea499e5fbe1e67101804e15d04c03db
Pietro <donapieppo@yahoo.it>
2020-10-15 09:23:35 +0200

    configure_authlevels

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -13,6 +13,12 @@ module Dummy
     config.load_defaults 6.0
     config.dm_unibo_common = ActiveSupport::HashWithIndifferentAccess.new config_for(:dm_unibo_common)
 
+    config.authlevels = { 
+      read:   1, 
+      manage: 2,
+      pippo: 3
+    }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
