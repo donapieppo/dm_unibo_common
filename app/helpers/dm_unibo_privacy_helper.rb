fb053952bed833bbfa831d36bff6508cc4e15e34
Pietro <donapieppo@yahoo.it>
2021-02-23 10:33:46 +0100

    br

--- app/helpers/dm_unibo_privacy_helper.rb
+++ app/helpers/dm_unibo_privacy_helper.rb
@@ -11,8 +11,8 @@ module DmUniboPrivacyHelper
       cookies[name] = { value: "accepted", expires: 1.year.from_now }
       raw %Q|
       <div class="alert alert-warning alert-dismissible" role="alert">
-        Questo sito utilizza solo cookie tecnici per il corretto funzionamento delle pagine web e per il miglioramento dei servizi. <br/>
-        Se vuoi saperne di più o negare il consenso consulta <a  href="http://www.unibo.it/it/cookies">l'informativa sulla privacy</a>. 
+        Questo sito utilizza solo cookie tecnici per il corretto funzionamento delle pagine web e per il miglioramento dei servizi.<br/>
+        Se vuoi saperne di più o negare il consenso consulta <a  href="http://www.unibo.it/it/cookies">l'informativa sulla privacy</a>.<br/> 
         Proseguendo la navigazione del sito o cliccando su "chiudi" acconsenti all'uso dei cookie.
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">&times; chiudi</span>
