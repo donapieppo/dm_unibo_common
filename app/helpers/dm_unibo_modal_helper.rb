41e136152c2119615c17397dac281872db843648
Pietro <donapieppo@yahoo.it>
2022-01-26 09:44:45 +0100

    modal

--- app/helpers/dm_unibo_modal_helper.rb
+++ app/helpers/dm_unibo_modal_helper.rb
@@ -31,27 +31,26 @@ module DmUniboModalHelper
     $('#main-modal .modal-body').load(url + separator + "modal=yyy");
     $('#main-modal').modal('show');
   });
-  |
+      |
     end
   end
 
   def bootstrap_modal_div
     raw %Q|
-      <div class="modal fade" id="main-modal" tabindex="-1" role="dialog" aria-labelledby="main_modal" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title"></h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true" style="font-size: 42px">&times;</span></button>
-            </div>
-            <div class="modal-body">
-            </div>
-            <div class="modal-footer">
-            </div>
-          </div><!-- .modal-content -->
-        </div><!-- .modal-dialog -->
-      </div><!-- .modal -->
-      |
+<div class="modal fade" id="mainModal" tabindex="-1" aria-labelledby="mainModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="mainModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      </div>
+      <div class="modal-footer">
+      </div>
+    </div>
+  </div>
+</div>
+    |
   end
-
 end
