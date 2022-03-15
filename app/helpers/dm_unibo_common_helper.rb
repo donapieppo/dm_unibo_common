401b24f1ca7823f47ea4e91f64f7856d2e1fdd87
Pietro <donapieppo@yahoo.it>
2022-03-15 09:26:13 +0100

    space

--- app/helpers/dm_unibo_common_helper.rb
+++ app/helpers/dm_unibo_common_helper.rb
@@ -21,7 +21,7 @@ module DmUniboCommonHelper
   end
 
   def fwdmicon(name, text: "", size: 18, prefix: 'fas')
-    raw "<i style=\"font-size: #{size}px\" class=\"#{prefix} fa-#{name} fa-fw\"></i> #{text}"
+    raw "<i style=\"font-size: #{size}px\" class=\"#{prefix} fa-#{name} fa-fw\"></i>#{text}"
   end
 
   def big_dmicon(name, text: "", size: 24, prefix: 'fas')
