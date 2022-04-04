c41fa4772fb67e3d330858671db34591232c0c7b
Pietro <donapieppo@yahoo.it>
2022-04-04 13:48:55 +0200

    modal-title

--- app/helpers/dm_unibo_modal_helper.rb
+++ app/helpers/dm_unibo_modal_helper.rb
@@ -41,7 +41,7 @@ module DmUniboModalHelper
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="mainModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="mainModalLabel"></h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
