97ff39e48ccb4f7ecd1da77d1eea4a75656e9fb5
Pietro <donapieppo@yahoo.it>
2022-05-31 11:40:44 +0200

    FINALLY assets precompiled images from engine

--- lib/dm_unibo_common/engine.rb
+++ lib/dm_unibo_common/engine.rb
@@ -9,5 +9,11 @@ module DmUniboCommon
       g.fixture_replacement :factory_bot
       g.factory_bot dir: 'spec/factories'
     end 
+
+    initializer "dm_unibo_common.assets.precompile" do |app|
+      app.config.assets.precompile += %w( dm_unibo_common/unibo.png dm_unibo_common/ssologo18x18.png ) 
+    end
   end
 end
+
+
