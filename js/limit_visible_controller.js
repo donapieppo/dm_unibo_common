6bae3e62fd0a0f502cfe347ef0960763429cd1ae
Pietro <donapieppo@yahoo.it>
2023-03-07 10:01:13 +0100

    display

--- js/limit_visible_controller.js
+++ js/limit_visible_controller.js
@@ -2,16 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 class LimitVisibleController extends Controller {
         static targets = ["item"]
+        static values = { display: String, default: 'block' }
 
         connect() {
-                console.log("dm_unibo_common limit visible connected")
+                console.log(`dm_unibo_common limit visible connected on ${display}`")
         }
 
         update(e) {
                 const input = e.target.value.trim();
                 console.log(`update with ${input}`)
                 this.itemTargets.forEach( (o) => {
-                        o.style.display = (o.dataset.text.includes(input)) ? 'flex' : 'none';
+                        o.style.display = (o.dataset.text.includes(input)) ? this.displayValue : 'none';
                 });
         }
 }
