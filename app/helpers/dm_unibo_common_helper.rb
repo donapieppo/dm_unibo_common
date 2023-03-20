2c3f96e3d3bfc5dfd7e3363f6c17a29f7835da08
Pietro <donapieppo@yahoo.it>
2023-03-20 11:27:22 +0100

    csrf_meta_tags.to_s csp_meta_tag.to_s for testing and syntax by ALE

--- app/helpers/dm_unibo_common_helper.rb
+++ app/helpers/dm_unibo_common_helper.rb
@@ -5,12 +5,12 @@ include DmUniboLinkHelper
 include DmUniboModalHelper
 include DmUniboIconHelper
 
-module DmUniboCommonHelper 
+module DmUniboCommonHelper
   def dm_unibo_common_headers
-    csrf_meta_tags +
-    csp_meta_tag +
-    stylesheet_link_tag("application", "data-turbo-track": "reload") +
-    javascript_include_tag("application", "data-turbo-track": "reload", defer: true) 
+    csrf_meta_tags.to_s +
+      csp_meta_tag.to_s +
+      stylesheet_link_tag("application", "data-turbo-track": "reload") +
+      javascript_include_tag("application", "data-turbo-track": "reload", defer: true)
   end
 
   def main_title(srt)
@@ -20,7 +20,7 @@ module DmUniboCommonHelper
 
   # If user is sso logged even if he has no access should see his eppn (and logout link)
   def sso_user_upn
-    request.env['HTTP_EPPN'] || (current_user and current_user.upn) || (current_user and current_user.email) 
+    request.env["HTTP_EPPN"] || (current_user && current_user.upn) || (current_user && current_user.email) 
   end
 
   # in confiiguration dm_unibo_common[:impersonate_admins] is array of upn/email that can
@@ -38,15 +38,14 @@ module DmUniboCommonHelper
 
       type = type.to_sym
       type = :success if type.to_s == :notice.to_s
-      type = :danger  if type.to_s == :alert.to_s
-      type = :danger  if type.to_s == :error.to_s
+      type = :danger if type.to_s == :alert.to_s
+      type = :danger if type.to_s == :error.to_s
       next unless ALERT_TYPES.include?(type)
 
-      i = (type == :danger) ? 'exclamation-triangle' : 'info-circle'
+      i = (type == :danger) ? "exclamation-triangle" : "info-circle"
 
       Array(message).each do |msg|
-        text = content_tag(:div, dmicon(i) + ' ' + msg.html_safe,  
-                           class: "alert alert-#{type}", role: 'alert')
+        text = content_tag(:div, dmicon(i) + " " + msg.html_safe, class: "alert alert-#{type}", role: "alert")
         flash_messages << text.html_safe if msg
       end
     end
@@ -56,7 +55,7 @@ module DmUniboCommonHelper
   def tooltip(key)
     message = Tooltip.message(key)
     raw %$<h3 class="info"> $ + image_tag("info.png", width: '15') + 
-        %$ #{message[0]} <span>#{message[1]}</span></h3>$
+      %$ #{message[0]} <span>#{message[1]}</span></h3>$
   end
 
   def popover_help(title, content)
@@ -70,28 +69,28 @@ module DmUniboCommonHelper
   # dl_field(:user_name, "Pietro)
   def dl_field(object, what)
     if object.is_a?(Symbol)
-      content_tag(:dt, I18n.t(object).capitalize) + 
-      content_tag(:dd, what)
+      content_tag(:dt, I18n.t(object).capitalize) +
+        content_tag(:dd, what)
     else
-      content_tag(:dt, I18n.t("activerecord.attributes.#{object.class.to_s.downcase}.#{what}").capitalize) + 
-      content_tag(:dd, object.send(what)) # what is a symbol
+      content_tag(:dt, I18n.t("activerecord.attributes.#{object.class.to_s.downcase}.#{what}").capitalize) +
+        content_tag(:dd, object.send(what)) # what is a symbol
     end
   end
 
-  def class_active_val(x,y)
-    (x.to_s.downcase == y.to_s.downcase) ? 'active' : ''
+  def class_active_val(x, y)
+    (x.to_s.downcase == y.to_s.downcase) ? "active" : ""
   end
 
-  def class_active(x,y)
-    (x.to_s.downcase == y.to_s.downcase) ? 'class="active"'.html_safe : ''
+  def class_active(x, y)
+    (x.to_s.downcase == y.to_s.downcase) ? 'class="active"'.html_safe : ""
   end
 
-  def dm_card(title: '', add_class: '')
+  def dm_card(title: "", add_class: "")
     content_tag :div, class: "dm-card #{add_class}" do
-      content_tag(:div, title, class: 'dm-card-title') +
-      content_tag(:div, class: 'dm-card-body') do
-        yield
-      end
+      content_tag(:div, title, class: "dm-card-title") +
+        content_tag(:div, class: "dm-card-body") do
+          yield
+        end
     end
   end
 
