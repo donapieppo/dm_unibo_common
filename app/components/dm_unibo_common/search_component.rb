ac1817c0cffc0161cb99abbcd7bedd914d1dc269
Pietro <donapieppo@yahoo.it>
2023-05-26 15:10:05 +0200

    search type

--- app/components/dm_unibo_common/search_component.rb
+++ app/components/dm_unibo_common/search_component.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class DmUniboCommon::SearchComponent < ViewComponent::Base
-  def initialize(search_path: nil, placeholder: "", title: "")
+  def initialize(search_path: nil, placeholder: "", title: "", type: "string")
     @search_path = search_path
     @placeholder = placeholder
     @title = title
+    @type = type
   end
 
   def render?
