b748e5f1ae0bc458e27b91297284930761ac7f01
Pietro <donapieppo@yahoo.it>
2023-05-26 16:24:31 +0200

    old moved in component

--- app/helpers/dm_unibo_menu_helper.rb
+++ app/helpers/dm_unibo_menu_helper.rb
@@ -22,27 +22,6 @@ module DmUniboMenuHelper
     # Rails.configuration.dm_unibo_common[:logout_link]
   end
 
-  def logged_user
-    content_tag :ul, class: "navbar-nav" do 
-      if sso_user_upn
-        %Q|<span class="navbar-text ps-1">#{sso_user_upn}</span>
-           <li>#{logout_link}</li>|.html_safe
-      else
-        %Q|<li>#{login_link}</li>|.html_safe
-      end
-    end
-  end
-
-  def dm_header(dm_header_title: Rails.configuration.header_title, dm_header_subtitle: Rails.configuration.header_subtitle)
-    main_root_path = current_organization ? main_app.current_organization_root_path : main_app.root_path
-
-    string = (dm_header_title) + content_tag(:div, dm_header_subtitle, style: 'font-size: 75%')
-
-    link_to(image_tag(Rails.configuration.dm_unibo_common[:logo_image]), Rails.configuration.dm_unibo_common[:logo_page], class: 'navbar-brand') +
-    link_to(big_dmicon(Rails.configuration.header_icon), main_root_path, class: 'navbar-brand navbar-icon', data: { turbo: false }) +
-    link_to(string.html_safe, main_root_path, class: 'navbar-brand', data: { turbo: false }) 
-  end
-
   def dropdown_menu(id, title, &block)
     raw %Q|
 <li class="nav-item dropdown">
