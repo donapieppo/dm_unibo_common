7bcb8846ffd67f27047d29997baee286f8056fb2
Pietro <donapieppo@yahoo.it>
2023-05-30 11:29:53 +0200

    home

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base
-  before_action :force_sso_user
-  after_action :verify_authorized, except: [:index, :who_impersonate, :impersonate, :shibboleth]
+class ApplicationController < DmUniboCommon::ApplicationController
+  before_action :set_current_user, :update_authorization, :set_current_organization, :log_current_user, :redirect_unsigned_user
+  after_action :verify_authorized, except: [:who_impersonate, :impersonate, :stop_impersonating]
 end
