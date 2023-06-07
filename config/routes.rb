bae320384253b90f157e3adb6cb138d3784b7803
Pietro <donapieppo@yahoo.it>
2023-06-07 09:54:32 +0200

    test

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ DmUniboCommon::Engine.routes.draw do
   get 'auth/google_oauth2/callback', to: 'logins#google_oauth2'
   get 'auth/shibboleth/callback',    to: 'logins#shibboleth'
   get 'auth/developer/callback',     to: 'logins#developer'
+  get 'auth/test/callback',          to: 'logins#test'
   get 'logins/logout',               to: 'logins#logout',    as: :logout
   get 'logins/no_access',            to: 'logins#no_access', as: :no_access
   get 'logins/pippo_show',           to: 'logins#pippo_show' 
