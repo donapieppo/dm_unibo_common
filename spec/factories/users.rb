ef12ce096ef3ef7ff2dcaa9ea9a86b79f21ce56a
Pietro <donapieppo@yahoo.it>
2023-06-07 09:54:05 +0200

    typo

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,8 @@
 FactoryBot.define do
   factory :user do
     sequence(:id) { |n| n }
-    upn  { 'nome.cognome@unibo.it' }
+    upn { "nome.cognome@unibo.it" }
     name { "Nome" }
     surname { "Cognome" }
   end
 end
-
-
-
