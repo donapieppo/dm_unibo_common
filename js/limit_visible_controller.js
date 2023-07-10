a86ce229c433db297cf7f92dcc339a22fb45f42c
Pietro <donapieppo@yahoo.it>
2023-07-10 16:48:27 +0200

    checkbox and substring

--- js/limit_visible_controller.js
+++ js/limit_visible_controller.js
@@ -3,19 +3,34 @@ import { Controller } from "@hotwired/stimulus"
 class LimitVisibleController extends Controller {
         static targets = ["item"]
         static values = { 
-                display: { type: String, default: 'block' }
+                substring: {type: Boolean, default: false},
+                display: {type: String, default: "block"},
         }
 
         connect() {
-                console.log(`dm_unibo_common limit visible connected on ${this.displayValue}`)
+                console.log(`dm_unibo_common limit visible connected on ${this.displayValue} with substring=${this.substringValue}`);
         }
 
         update(e) {
-                const input = e.target.value.trim().toLowerCase();
+                var input;
+                var check;
+
+                if (e.target.type == "checkbox") {
+                        input = e.target.checked.toString();
+                } else {
+                        input = e.target.value.trim().toLowerCase();
+                }
+                
                 console.log(`update with ${input}`)
+
                 this.itemTargets.forEach((o) => {
                         if ('text' in o.dataset) {
-                                o.style.display = (o.dataset.text === input) ? this.displayValue : 'none';
+                                if (this.substringValue) {
+                                        check = o.dataset.text.includes(input)
+                                } else {
+                                        check = o.dataset.text == input
+                                }
+                                o.style.display = check ? this.displayValue : 'none';
                         }
                 });
         }
