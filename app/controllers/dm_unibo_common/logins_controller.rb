76c18e855e38547620916ecf6e78a2c75e359958
Pietro <donapieppo@yahoo.it>
2023-07-12 10:47:44 +0200

    rubocop

--- app/controllers/dm_unibo_common/logins_controller.rb
+++ app/controllers/dm_unibo_common/logins_controller.rb
@@ -24,142 +24,152 @@
 #
 # see lib/dm_unibo_common/controllers/helpers.rb for method definitions.
 module DmUniboCommon
-class LoginsController < ::ApplicationController
-  # raise: false see http://api.rubyonrails.org/classes/ActiveSupport/Callbacks/ClassMethods.html#method-i-skip_callback
-  skip_before_action :force_sso_user, :redirect_unsigned_user, :check_role, :retrive_authlevel, :after_current_user_and_organization, raise: false
-  skip_after_action :verify_authorized
+  class LoginsController < ::ApplicationController
+    # raise: false see http://api.rubyonrails.org/classes/ActiveSupport/Callbacks/ClassMethods.html#method-i-skip_callback
+    skip_before_action :force_sso_user, :redirect_unsigned_user, :check_role, :retrive_authlevel, :after_current_user_and_organization, raise: false
+    skip_after_action :verify_authorized
 
-  # env['omniauth.auth'].info = {email, name, last_name}
-  def google_oauth2
-    Rails.configuration.dm_unibo_common[:omniauth_provider] == :google_oauth2 or raise
-    parse_google_omniauth
-    send login_method
-  end
-
-  # email="usrBase@testtest.unibo.it" last_name="Base" name="SSO"
-  def shibboleth
-    Rails.configuration.dm_unibo_common[:omniauth_provider] == :shibboleth or raise
-    log_unibo_omniauth
-    parse_unibo_omniauth
-
-    if Rails.configuration.dm_unibo_common[:no_students] && @email !~ /@unibo.it$/
-      logger.info "Students are not allowed: #{@email} user not allowed."
-      redirect_to no_access_path and return
-    else
+    # env['omniauth.auth'].info = {email, name, last_name}
+    def google_oauth2
+      Rails.configuration.dm_unibo_common[:omniauth_provider] == :google_oauth2 or raise DmUniboCommon::WrongOauthMethod
+      parse_google_omniauth
       send login_method
     end
-  end
 
-  def developer
-    Rails.configuration.dm_unibo_common[:omniauth_provider] == :developer or raise
-    request.remote_ip == '127.0.0.1' or request.remote_ip == '::1' or request.remote_ip =~ /^172\.\d+\.\d+\.\d+/ or raise "ONLY LOCAL OF DOCKER. YOU ARE #{request.remote_ip}"
-    sign_in_and_redirect ::User.find(Rails.configuration.dm_unibo_common[:omniauth_developer_user_id])
-  end
+    # email="usrBase@testtest.unibo.it" last_name="Base" name="SSO"
+    def shibboleth
+      Rails.configuration.dm_unibo_common[:omniauth_provider] == :shibboleth or raise DmUniboCommon::WrongOauthMethod
+      log_unibo_omniauth
+      parse_unibo_omniauth
 
-  def test
-    Rails.configuration.dm_unibo_common[:omniauth_provider] == :test or raise
-    request.remote_ip == '127.0.0.1' or request.remote_ip == '::1' or request.remote_ip =~ /^172\.\d+\.\d+\.\d+/ or raise "ONLY LOCAL OF DOCKER. YOU ARE #{request.remote_ip}"
-    sign_in_and_redirect ::User.find_by_id(params[:user_id_id])
-  end
-
-  # example ["_shibsession_lauree", "_affcf2ffbe098d5a0928dc72cd9de489"]
-  #         ["_lauree_session", "YU5RSTM2OXdYMkRyVjV0SXI1K3c3eDJJdjZQ..... "]
-  def logout
-    # cookies.delete(Rails.configuration.session_options[:key].to_sym)
-    # cookies.delete(shibapplicationid.to_sym)
-    session[:user_id] = nil
-    cookies.clear
-    reset_session
-    logger.info("after logout we redirect to params[:return] = #{params[:return]}")
-    redirect_to Rails.configuration.dm_unibo_common[:logout_link], allow_other_host: true
-    # redirect_to (params[:return] || 'http://www.unibo.it')
-  end
-
-  # Not authorized but valid credentials
-  def no_access
-    render layout: nil
-  end
-
-  def pippo_show
-    # raise env.inspect
-  end
-
-  private
-
-  # the default is conservative where you log only if user in database
-  def login_method
-    Rails.configuration.dm_unibo_common[:login_method] || "allow_if_email"
-  end
-
-  def parse_google_omniauth
-    oinfo = request.env['omniauth.auth'].info
-    @email   = oinfo.email
-    @name    = oinfo.name
-    @surname = oinfo.last_name
-  end
-
-  def parse_unibo_omniauth
-    @upn  = request.env['omniauth.auth'].uid
-    oinfo = request.env['omniauth.auth'].info
-    extra = request.env['omniauth.auth'].extra.raw_info
-
-    @idAnagraficaUnica = extra.idAnagraficaUnica.to_i
-    @idAnagraficaUnica > 0 or raise "NO idAnagraficaUnica"
-
-    @isMemberOf = extra.isMemberOf ? extra.isMemberOf.split(';') : []
-    set_memberof_session(@isMemberOf)
-
-    @email         = @upn
-    @name          = oinfo.first_name || oinfo.name
-    @surname       = oinfo.last_name
-    @nationalpin   = extra.codiceFiscale
-  end
-
-  def set_memberof_session(isMemberOf)
-    (Rails.env.development? and isMemberOf << 'user') unless isMemberOf.include?('user')
-    session[:isMemberOf] = isMemberOf
-  end
-
-  def allow_and_create
-    user = @idAnagraficaUnica ? ::User.where(id: @idAnagraficaUnica).first : ::User.where(email: @email).first
-    if ! user
-      logger.info "Authentication: User #{@email} to be CREATED"
-      h = {id:      @idAnagraficaUnica || 0,
-           upn:     @email,
-           email:   @email,
-           name:    @name, 
-           surname: @surname }
-      h[:nationalpin] = @nationalpin if ::User.column_names.include?('nationalpin')
-      user = ::User.create!(h)
+      if Rails.configuration.dm_unibo_common[:no_students] && @email !~ /@unibo.it$/
+        logger.info "Students are not allowed: #{@email} user not allowed."
+        redirect_to no_access_path and return
+      else
+        send login_method
+      end
     end
-    logger.info "Authentication: allow_and_create as #{user.inspect} with groups #{session[:isMemberOf].inspect}"
-    sign_in_and_redirect user
-  end
-  alias_method :log_and_create, :allow_and_create # old syntax 
 
-  def allow_if_email
-    user = @idAnagraficaUnica ? ::User.where(id: @idAnagraficaUnica).first : ::User.where(email: @email).first
-    if user
-      logger.info "Authentication: allow_if_email as #{user.inspect} with groups #{session[:isMemberOf].inspect}"
-      user.update(name: @name, surname: @surname)
+    def developer
+      Rails.configuration.dm_unibo_common[:omniauth_provider] == :developer or raise DmUniboCommon::WrongOauthMethod
+      if request.remote_ip == "127.0.0.1" || request.remote_ip == "::1" || request.remote_ip =~ /^172\.\d+\.\d+\.\d+/
+        sign_in_and_redirect ::User.find(Rails.configuration.dm_unibo_common[:omniauth_developer_user_id])
+      else
+        raise "ONLY LOCAL OF DOCKER. YOU ARE #{request.remote_ip}"
+      end
+    end
+
+    def test
+      Rails.configuration.dm_unibo_common[:omniauth_provider] == :test or raise
+      if request.remote_ip == "127.0.0.1" || request.remote_ip == "::1" || request.remote_ip =~ /^172\.\d+\.\d+\.\d+/
+        user = ::User.find(params[:user_id_id])
+        Rails.logger.info("#{params[:user_id_id]} -> #{user.inspect}")
+        sign_in_and_redirect ::User.find(params[:user_id_id])
+      else
+        raise "ONLY LOCAL OF DOCKER. YOU ARE #{request.remote_ip}"
+      end
+    end
+
+    # example ["_shibsession_lauree", "_affcf2ffbe098d5a0928dc72cd9de489"]
+    #         ["_lauree_session", "YU5RSTM2OXdYMkRyVjV0SXI1K3c3eDJJdjZQ..... "]
+    def logout
+      # cookies.delete(Rails.configuration.session_options[:key].to_sym)
+      # cookies.delete(shibapplicationid.to_sym)
+      session[:user_id] = nil
+      cookies.clear
+      reset_session
+      logger.info("after logout we redirect to params[:return] = #{params[:return]}")
+      redirect_to Rails.configuration.dm_unibo_common[:logout_link], allow_other_host: true
+      # redirect_to (params[:return] || 'http://www.unibo.it')
+    end
+
+    # Not authorized but valid credentials
+    def no_access
+      render layout: nil
+    end
+
+    def pippo_show
+      # raise env.inspect
+    end
+
+    private
+
+    # the default is conservative where you log only if user in database
+    def login_method
+      Rails.configuration.dm_unibo_common[:login_method] || "allow_if_email"
+    end
+
+    def parse_google_omniauth
+      oinfo = request.env["omniauth.auth"].info
+      @email = oinfo.email
+      @name = oinfo.name
+      @surname = oinfo.last_name
+    end
+
+    def parse_unibo_omniauth
+      @upn = request.env["omniauth.auth"].uid
+      oinfo = request.env["omniauth.auth"].info
+      extra = request.env["omniauth.auth"].extra.raw_info
+
+      @idAnagraficaUnica = extra.idAnagraficaUnica.to_i
+      @idAnagraficaUnica > 0 or raise "NO idAnagraficaUnica"
+
+      @isMemberOf = extra.isMemberOf ? extra.isMemberOf.split(";") : []
+      set_memberof_session(@isMemberOf)
+
+      @email = @upn
+      @name = oinfo.first_name || oinfo.name
+      @surname = oinfo.last_name
+      @nationalpin = extra.codiceFiscale
+    end
+
+    def set_memberof_session(isMemberOf)
+      (Rails.env.development? and isMemberOf << 'user') unless isMemberOf.include?('user')
+      session[:isMemberOf] = isMemberOf
+    end
+
+    def allow_and_create
+      user = @idAnagraficaUnica ? ::User.where(id: @idAnagraficaUnica).first : ::User.where(email: @email).first
+      if !user
+        logger.info "Authentication: User #{@email} to be CREATED"
+        h = {
+          id: @idAnagraficaUnica || 0,
+          upn: @email,
+          email: @email,
+          name: @name,
+          surname: @surname
+        }
+        h[:nationalpin] = @nationalpin if ::User.column_names.include?("nationalpin")
+        user = ::User.create!(h)
+      end
+      logger.info "Authentication: allow_and_create as #{user.inspect} with groups #{session[:isMemberOf].inspect}"
       sign_in_and_redirect user
-    else
-      logger.info "User #{@email} not allowed"
-      redirect_to no_access_path
+    end
+    alias_method :log_and_create, :allow_and_create # old syntax
+
+    def allow_if_email
+      user = @idAnagraficaUnica ? ::User.where(id: @idAnagraficaUnica).first : ::User.where(email: @email).first
+      if user
+        logger.info "Authentication: allow_if_email as #{user.inspect} with groups #{session[:isMemberOf].inspect}"
+        user.update(name: @name, surname: @surname)
+        sign_in_and_redirect user
+      else
+        logger.info "User #{@email} not allowed"
+        redirect_to no_access_path
+      end
+    end
+    alias_method :log_if_email, :allow_if_email # old syntax
+
+    def log_unibo_omniauth
+      request.env["omniauth.auth"] or return
+      logger.info("Authentication: uid  = #{request.env["omniauth.auth"].uid}")
+      logger.info("Authentication: info = #{request.env["omniauth.auth"].info}")
+      logger.info("Authentication: extra = #{request.env["omniauth.auth"].extra}")
+    end
+
+    def sign_in_and_redirect(user)
+      session[:user_id] = user.id
+      redirect_to session[:original_request] || main_app.root_path
     end
   end
-  alias_method :log_if_email, :allow_if_email # old syntax 
-
-  def log_unibo_omniauth
-    request.env['omniauth.auth'] or return
-    logger.info("Authentication: uid   = #{request.env['omniauth.auth'].uid}")
-    logger.info("Authentication: info  = #{request.env['omniauth.auth'].info}")
-    logger.info("Authentication: extra = #{request.env['omniauth.auth'].extra}")
-  end
-
-  def sign_in_and_redirect(user)
-    session[:user_id] = user.id
-    redirect_to session[:original_request] || main_app.root_path
-  end
-end
 end
