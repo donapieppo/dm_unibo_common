a1be1b8e4c4fd8f82e3e168ade611a8eec2f6eb3
Pietro <donapieppo@yahoo.it>
2023-07-12 10:47:29 +0200

    WrongOauthMethod error + rubocop

--- lib/dm_unibo_common/errors.rb
+++ lib/dm_unibo_common/errors.rb
@@ -1,20 +1,23 @@
 module DmUniboCommon
-  class NotOwner             < RuntimeError; end
-  class NotAuthorized        < RuntimeError; end
-  class NoAccess             < RuntimeError; end
+  class NotOwner < RuntimeError; end
+
+  class NotAuthorized < RuntimeError; end
+
+  class NoAccess < RuntimeError; end
+
   class MismatchOrganization < RuntimeError; end
 
-  class NoUser       < RuntimeError
+  class NoUser < RuntimeError
     def to_s
-      I18n.t 'dm_unibo_common.no_user'
+      I18n.t "dm_unibo_common.no_user"
     end
   end
+
   class TooManyUsers < RuntimeError
     def to_s
-      I18n.t 'dm_unibo_common.too_many_users'
+      I18n.t "dm_unibo_common.too_many_users"
     end
   end
+
+  class WrongOauthMethod < RuntimeError; end
 end
-
-
-
