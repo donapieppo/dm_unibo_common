13a0ecb5334f0da6ae08a6010b3cf084dfd38f67
Pietro <donapieppo@yahoo.it>
2023-07-12 10:17:46 +0200

    rubocop and off with omniauth-shibboleth omniauth-google-oauth2 moved to single app config

--- lib/dm_unibo_common.rb
+++ lib/dm_unibo_common.rb
@@ -2,12 +2,12 @@ require "dm_unibo_common/version"
 require "dm_unibo_common/engine"
 
 # rails
-require 'mysql2'
+require "mysql2"
 
 # auth
 require "omniauth"
-require "omniauth-shibboleth"
-require "omniauth-google-oauth2"
+# require "omniauth-shibboleth"
+# require "omniauth-google-oauth2"
 require "pretender"
 require "pundit"
 require "view_component"
@@ -15,24 +15,24 @@ require "turbo-rails"
 require "stimulus-rails"
 require "simple_form"
 
-#ActiveSupport.on_load(:'turbo-rails') do
-#  include Turbo::FramesHelper
-#end
+# ActiveSupport.on_load(:'turbo-rails') do
+#   include Turbo::FramesHelper
+# end
 
 # self
-require 'dm_unibo_common/errors'
-require 'dm_unibo_common/development_mail_interceptor'
-require 'dm_unibo_common/user'
-require 'dm_unibo_common/organization'
-require 'dm_unibo_common/authorization'
-# require 'dm_unibo_common/permission'
-require 'dm_unibo_common/user_upn_methods'
-require 'dm_unibo_common/controllers/helpers'
+require "dm_unibo_common/errors"
+require "dm_unibo_common/development_mail_interceptor"
+require "dm_unibo_common/user"
+require "dm_unibo_common/organization"
+require "dm_unibo_common/authorization"
+# require "dm_unibo_common/permission"
+require "dm_unibo_common/user_upn_methods"
+require "dm_unibo_common/controllers/helpers"
 
-#require 'dm_unibo_common/policies/application_policy'
-#require 'dm_unibo_common/policies/organization_policy'
-#require 'dm_unibo_common/policies/permission_policy'
-#require 'dm_unibo_common/policies/impersonation_policy'
+# require 'dm_unibo_common/policies/application_policy'
+# require 'dm_unibo_common/policies/organization_policy'
+# require 'dm_unibo_common/policies/permission_policy'
+# require 'dm_unibo_common/policies/impersonation_policy'
 
 module DmUniboCommon
 end
