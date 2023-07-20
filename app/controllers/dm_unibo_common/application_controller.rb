fef956c97c22db90d79813d71ab8caea6c070fcc
Pietro <donapieppo@yahoo.it>
2023-07-20 11:53:25 +0200

    after_current_user_and_organization

--- app/controllers/dm_unibo_common/application_controller.rb
+++ app/controllers/dm_unibo_common/application_controller.rb
@@ -5,12 +5,15 @@ module DmUniboCommon
 
     include DmUniboCommon::Controllers::Helpers
 
-    #impersonates :user, method: :current_user, with: ->(id) { DmUniboCommon::CurrentUser.find(id) }
+    # impersonates :user, method: :current_user, with: ->(id) { DmUniboCommon::CurrentUser.find(id) }
     impersonates :user, method: :current_user, with: ->(id) { ::User.find(id) }
 
-    def default_url_options(_options={})
+    def default_url_options(_options = {})
       _options[:__org__] = current_organization ? current_organization.code : nil
       _options
+    end
+
+    def after_current_user_and_organization
     end
   end
 end
