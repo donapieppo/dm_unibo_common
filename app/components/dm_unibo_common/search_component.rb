93bbb566d1bced90f00ce78370e8a7c9a701359c
Pietro <donapieppo@yahoo.it>
2023-07-24 12:06:15 +0200

    autofocus

--- app/components/dm_unibo_common/search_component.rb
+++ app/components/dm_unibo_common/search_component.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class DmUniboCommon::SearchComponent < ViewComponent::Base
-  def initialize(search_path: nil, placeholder: "", title: "", type: "string")
+  def initialize(search_path: nil, placeholder: "", title: "", type: "string", autofocus: false)
     @search_path = search_path
     @placeholder = placeholder
     @title = title
     @type = type
+    @autofocus = autofocus
   end
 
   def render?
