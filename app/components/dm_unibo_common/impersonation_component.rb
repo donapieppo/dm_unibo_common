a5c3df234e410d3d91eae60bed21dc85c56891f7
Pietro <donapieppo@yahoo.it>
2023-07-25 11:17:21 +0200

    better to get who_impersonate_path and stop_impersonating_path from the app

--- app/components/dm_unibo_common/impersonation_component.rb
+++ app/components/dm_unibo_common/impersonation_component.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class DmUniboCommon::ImpersonationComponent < ViewComponent::Base
-  def initialize(current_user, true_user)
+  def initialize(current_user, true_user, who_impersonate_path: "", stop_impersonating_path: "")
     @current_user = current_user
     @true_user = true_user
+    @who_impersonate_path = who_impersonate_path
+    @stop_impersonating_path = stop_impersonating_path
     @can_impersonate = @true_user && Rails.configuration.dm_unibo_common[:impersonate_admins] && Rails.configuration.dm_unibo_common[:impersonate_admins].include?(@true_user.upn)
   end
 
