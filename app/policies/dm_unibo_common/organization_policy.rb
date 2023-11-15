caa0372d057d95507dcd57e0858b9e4f9beb3bf4
Pietro <donapieppo@yahoo.it>
2023-11-15 13:48:09 +0100

    rubocop

--- app/policies/dm_unibo_common/organization_policy.rb
+++ app/policies/dm_unibo_common/organization_policy.rb
@@ -1,30 +1,30 @@
 class DmUniboCommon::OrganizationPolicy < DmUniboCommon::ApplicationPolicy
   def index?
-    @user && @user.is_cesia?
+    @user&.is_cesia?
   end
 
   def show?
-    @user && @user.is_cesia?
+    @user&.is_cesia?
   end
 
   def create?
-    @user && @user.is_cesia?
+    @user&.is_cesia?
   end
 
   def update?
-    @user && @user.is_cesia?
+    @user&.is_cesia?
   end
 
-  class <<self
+  class << self
     # read? only_read?
     # manage? only_manage?
     def configure_authlevels
       Rails.configuration.authlevels.each do |name, number|
-        define_method :"#{name}?" do 
+        define_method :"#{name}?" do
           @user && @user.authorization.send("can_#{name}?", @record)
         end
 
-        define_method :"only_#{name}?" do 
+        define_method :"only_#{name}?" do
           @user && @user.authorization.send("can_only_#{name}?", @record)
         end
       end
