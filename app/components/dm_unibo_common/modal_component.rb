e56134068a83a60fba6bd73cce523003e7978609
Pietro <donapieppo@yahoo.it>
2023-11-15 13:48:21 +0100

    rubocop

--- app/components/dm_unibo_common/modal_component.rb
+++ app/components/dm_unibo_common/modal_component.rb
@@ -10,10 +10,9 @@ class DmUniboCommon::ModalComponent < ViewComponent::Base
   def initialize(title: "", hidden: false, noactions: false)
     @title = title
     @hidden = hidden
-    @actions = noactions ? "" : "turbo:click->turbo-modal#followLink 
+    @actions = noactions ? "" : "turbo:click->turbo-modal#followLink
                                  keyup@window->turbo-modal#closeWithKeyboard
                                  click@window->turbo-modal#closeBackground
-                                 turbo:submit-end->turbo-modal#submitEnd" 
+                                 turbo:submit-end->turbo-modal#submitEnd"
   end
 end
-
