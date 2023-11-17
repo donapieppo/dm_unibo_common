5b2179360b003294815613beafe1b650e4a04107
Pietro <donapieppo@yahoo.it>
2023-11-17 09:46:11 +0100

    verify_authorized

--- app/controllers/dm_unibo_common/application_controller.rb
+++ app/controllers/dm_unibo_common/application_controller.rb
@@ -1,16 +1,18 @@
 module DmUniboCommon
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    include Pundit::Authorization
 
+    include Pundit::Authorization
     include DmUniboCommon::Controllers::Helpers
+
+    after_action :verify_authorized
 
     # impersonates :user, method: :current_user, with: ->(id) { DmUniboCommon::CurrentUser.find(id) }
     impersonates :user, method: :current_user, with: ->(id) { ::User.find(id) }
 
-    def default_url_options(_options = {})
-      _options[:__org__] = current_organization ? current_organization.code : nil
-      _options
+    def default_url_options(o = {})
+      o[:__org__] = current_organization ? current_organization.code : nil
+      o
     end
 
     def after_current_user_and_organization
