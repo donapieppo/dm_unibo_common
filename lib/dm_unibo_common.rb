c493d04acd29040a6e88ac09fd6ffd2424009637
Pietro <donapieppo@yahoo.it>
2023-11-27 15:40:24 +0100

    clean

--- lib/dm_unibo_common.rb
+++ lib/dm_unibo_common.rb
@@ -6,19 +6,13 @@ require "mysql2"
 
 # auth
 require "omniauth"
-# require "omniauth-shibboleth"
-# require "omniauth-google-oauth2"
 require "pretender"
 require "pundit"
 require "view_component"
 require "turbo-rails"
 require "stimulus-rails"
 require "simple_form"
-require 'sprockets/railtie'
-
-# ActiveSupport.on_load(:'turbo-rails') do
-#   include Turbo::FramesHelper
-# end
+require "sprockets/railtie"
 
 # self
 require "dm_unibo_common/errors"
@@ -26,14 +20,8 @@ require "dm_unibo_common/development_mail_interceptor"
 require "dm_unibo_common/user"
 require "dm_unibo_common/organization"
 require "dm_unibo_common/authorization"
-# require "dm_unibo_common/permission"
 require "dm_unibo_common/user_upn_methods"
 require "dm_unibo_common/controllers/helpers"
-
-# require 'dm_unibo_common/policies/application_policy'
-# require 'dm_unibo_common/policies/organization_policy'
-# require 'dm_unibo_common/policies/permission_policy'
-# require 'dm_unibo_common/policies/impersonation_policy'
 
 module DmUniboCommon
 end
