774d839edb9b1f4e5f039a8df531e3ea5041ef8b
Pietro <donapieppo@yahoo.it>
2024-06-11 15:56:08 +0200

    smtp_unibo

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,21 +1,21 @@
 Rails.application.reloader.to_prepare do
-  if Rails.configuration.dm_unibo_common[:smtp_unibo] 
-    ActionMailer::Base.smtp_settings = {
-      address:        'mail.unibo.it',
-      smtp_domain:    'unibo.it',
+  ActionMailer::Base.smtp_settings = if Rails.configuration.dm_unibo_common[:smtp_unibo]
+    {
+      address: "mail.unibo.it",
+      smtp_domain: "unibo.it",
       enable_starttls_auto: true,
-      port:           587,
-      user_name:      ENV['UNIBO_SMTP_USERNAME'],
-      password:       ENV['UNIBO_SMTP_PASSWORD'],
+      port: 587,
+      user_name: ENV["SMTP_USERNAME"],
+      password: ENV["SMTP_PASSWORD"],
       authentication: :login
     }
   else
-    ActionMailer::Base.smtp_settings = { 
-      address: Rails.configuration.dm_unibo_common[:smtp_address], 
-      domain:  Rails.configuration.dm_unibo_common[:smtp_domain] 
+    {
+      address: Rails.configuration.dm_unibo_common[:smtp_address],
+      domain: Rails.configuration.dm_unibo_common[:smtp_domain]
     }
   end
 
-  ActionMailer::Base.register_interceptor(DmUniboCommon::DevelopmentMailInterceptor) 
+  ActionMailer::Base.register_interceptor(DmUniboCommon::DevelopmentMailInterceptor)
   Rails.application.routes.default_url_options[:host] = Rails.configuration.dm_unibo_common[:host]
 end
