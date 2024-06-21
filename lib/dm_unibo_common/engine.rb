95a8d7a71374b2c413501e184876b8f7df99fc93
Pietro <donapieppo@yahoo.it>
2024-06-21 08:58:07 +0200

    engine

--- lib/dm_unibo_common/engine.rb
+++ lib/dm_unibo_common/engine.rb
@@ -7,13 +7,11 @@ module DmUniboCommon
     config.generators do |g|
       g.test_framework :rspec
       g.fixture_replacement :factory_bot
-      g.factory_bot dir: 'spec/factories'
-    end 
+      g.factory_bot dir: "spec/factories"
+    end
 
     initializer "dm_unibo_common.assets.precompile" do |app|
-      app.config.assets.precompile += %w( dm_unibo_common/unibo.png dm_unibo_common/ssologo18x18.png ) 
+      app.config.assets.precompile += %w[dm_unibo_common/unibo.png dm_unibo_common/ssologo18x18.png]
     end
   end
 end
-
-
