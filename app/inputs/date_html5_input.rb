220e68b5596878fe8a48815a85a5656dfee89d88
Pietro <donapieppo@yahoo.it>
2024-09-20 15:05:05 +0200

    rubocop

--- app/inputs/date_html5_input.rb
+++ app/inputs/date_html5_input.rb
@@ -1,5 +1,5 @@
 # attribute_name: :date
-# object: 
+# object:
 # input_html_options: {:class=>[:datetime_picker2, :required], :required=>false, :"aria-required"=>nil, :"aria-invalid"=>nil}
 # input_options: {:valid_class=>"form-group-valid", :maxlength=>false, :minlength=>false, :pattern=>false, :min_max=>false, :readonly=>false, :placeholder=>false, :as=>:datetime_picker2}
 class DateHtml5Input < SimpleForm::Inputs::Base
@@ -8,12 +8,11 @@ class DateHtml5Input < SimpleForm::Inputs::Base
   end
 
   def input(wrapper_options)
-    template.content_tag(:div, class: 'input-group date', style: 'width: 75%') do
-      template.concat @builder.date_field(attribute_name, 
-                                           input_html_options.merge(value: attr_day, 
-                                                                    class: 'form-control', 
-                                                                    html5: true))
+    template.content_tag(:div, class: "input-group date", style: "width: 75%") do
+      template.concat @builder.date_field(
+        attribute_name,
+        input_html_options.merge(value: attr_day, class: "form-control", html5: true)
+      )
     end
   end
 end
-
