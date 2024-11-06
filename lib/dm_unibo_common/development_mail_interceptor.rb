7ff1ee156fc9c8a733d751e770cfef88cfc3e25e
Pietro <donapieppo@yahoo.it>
2024-11-06 09:28:14 +0100

    rubocop

--- lib/dm_unibo_common/development_mail_interceptor.rb
+++ lib/dm_unibo_common/development_mail_interceptor.rb
@@ -3,22 +3,20 @@
 # An interceptor object must implement the :delivering_email(message) method which will be called before the email is sent, allowing you to make modifications to the email before it hits the delivery agents. Your object should make and needed modifications directly to the passed in Mail::Message instance.
 
 module DmUniboCommon
-class DevelopmentMailInterceptor  
-  def self.delivering_email(message)  
-    if Rails.env.development?
-      Rails.logger.info("modify recipient mail")
-      message.subject = "[to :#{message.to} cc:#{message.cc} bcc:#{message.bcc}] #{message.subject}"
-      message.to  = Rails.configuration.dm_unibo_common[:interceptor_mails]
-      message.cc  = nil
-      message.bcc = nil
-    else
-      if ! message.bcc.kind_of?(Array)
+  class DevelopmentMailInterceptor
+    def self.delivering_email(message)
+      if Rails.env.development?
+        Rails.logger.info("Modify recipient mail.")
+        message.subject = "[to :#{message.to} cc:#{message.cc} bcc:#{message.bcc}] #{message.subject}"
+        message.to = Rails.configuration.dm_unibo_common[:interceptor_mails]
+        message.cc = nil
+        message.bcc = nil
+      elsif !message.bcc.is_a?(Array)
         message.bcc = message.bcc ? [message.bcc] : []
       end
+      if Rails.configuration.dm_unibo_common[:message_footer] && !Rails.configuration.dm_unibo_common[:message_footer].blank?
+        message.body = message.body.to_s + "\n ------------------- \n" + Rails.configuration.dm_unibo_common[:message_footer]
+      end
     end
-    if Rails.configuration.dm_unibo_common[:message_footer] and ! Rails.configuration.dm_unibo_common[:message_footer].blank?
-      message.body = message.body.to_s + "\n ------------------- \n" + Rails.configuration.dm_unibo_common[:message_footer]
-    end
-  end  
-end  
+  end
 end
