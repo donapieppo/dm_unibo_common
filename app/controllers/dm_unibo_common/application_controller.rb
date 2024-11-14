9ce0f3365aa94d55f4bc79bbc234fc9d9e2ab545
Pietro <donapieppo@yahoo.it>
2024-11-14 10:16:21 +0100

    rescue_from Pundit::NotAuthorizedError

--- app/controllers/dm_unibo_common/application_controller.rb
+++ app/controllers/dm_unibo_common/application_controller.rb
@@ -10,12 +10,21 @@ module DmUniboCommon
     # impersonates :user, method: :current_user, with: ->(id) { DmUniboCommon::CurrentUser.find(id) }
     impersonates :user, method: :current_user, with: ->(id) { ::User.find(id) }
 
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
     def default_url_options(o = {})
       o[:__org__] = current_organization ? current_organization.code : nil
       o
     end
 
     def after_current_user_and_organization
+    end
+
+    private
+
+    def user_not_authorized
+      flash[:alert] = "Non siete abilitati ad accedere alla pagina."
+      redirect_to(home_path)
     end
   end
 end
