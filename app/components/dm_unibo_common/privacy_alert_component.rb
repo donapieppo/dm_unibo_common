5b4022c38121fa9002eac8e045c8a9a7beb463dc
Pietro <donapieppo@yahoo.it>
2024-11-19 15:40:19 +0100

    DM_UNIBO_COMMON_PRIVACY_KEY

--- app/components/dm_unibo_common/privacy_alert_component.rb
+++ app/components/dm_unibo_common/privacy_alert_component.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
-DM_UNIBO_COMMON_PRIVACY_KEY = (Rails.configuration.session_options[:key] + "_privacy").to_sym
-
 class DmUniboCommon::PrivacyAlertComponent < ViewComponent::Base
   def initialize(cookies)
-    @privacy_accepted = cookies[DM_UNIBO_COMMON_PRIVACY_KEY]
-    if !@privacy_accepted
-      cookies[DM_UNIBO_COMMON_PRIVACY_KEY] = {value: "accepted", expires: 1.year.from_now}
+    dm_unibo_common_privacy_key = (Rails.configuration.session_options[:key] + "_privacy").to_sym
+
+    if !(@privacy_accepted = cookies[dm_unibo_common_privacy_key])
+      cookies[dm_unibo_common_privacy_key] = {value: "accepted", expires: 1.year.from_now}
     end
   end
 
