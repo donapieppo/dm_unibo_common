73865b175a00226bf39a551418a82238837be367
Pietro <donapieppo@yahoo.it>
2025-01-30 14:46:48 +0100

    contact_mail

--- app/helpers/dm_unibo_common_helper.rb
+++ app/helpers/dm_unibo_common_helper.rb
@@ -53,10 +53,14 @@ module DmUniboCommonHelper
   end
 
   def mail_to_contact
-    mail_to Rails.configuration.contact_mail, Rails.configuration.contact_mail
+    mail_to Rails.configuration.unibo_common.contact_mail, Rails.configuration.unibo_common.contact_mail
   end
 
   def check_user_is_cesia
     current_user.is_cesia? or raise DmUniboCommon::NotAuthorized, "Non sufficienti privilegi per seguire l'operazione"
+  end
+
+  def auth_callback_path
+    dm_unibo_common.send(:"auth_#{Rails.configuration.unibo_common.omniauth_provider}_callback_path")
   end
 end
