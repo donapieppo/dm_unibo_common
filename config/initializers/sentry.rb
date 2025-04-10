751d6adc2feeb637915d0761e3b0e2862991ab58
Pietro <donapieppo@yahoo.it>
2025-04-10 12:09:24 +0200

    sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,7 +5,8 @@ if Rails.configuration.unibo_common.sentry_dsn
     # get breadcrumbs from logs
     config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-    config.level = :warning
+    # NO
+    # config.level = :warning
 
     # Add data like request headers and IP for users, if applicable;
     # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
