a0a585e7db964dc447ab9f54472e1d65e3ced499
Pietro <donapieppo@yahoo.it>
2025-08-06 11:10:22 +0200

    rubocop

--- app/models/dm_unibo_common/permission.rb
+++ app/models/dm_unibo_common/permission.rb
@@ -15,7 +15,7 @@ class Permission < ApplicationRecord
   belongs_to :user, class_name: "::User", optional: true
 
   validates :organization, presence: true
-  validates :network, format: {with: /\A\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/i, message: "Formato corretto di network: 137.204.13.0.", allow_nil: true}
+  validates :network, format: {with: /\A\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\z/i, message: "Formato corretto di network: 137.204.13.0.", allow_nil: true}
   validates :authlevel, inclusion: {in: ::Authorization.all_authlevels.values,
                                     message: "Errore interno al sistema su authlevel. Contattare Assistenza."}
 
@@ -31,7 +31,7 @@ class Permission < ApplicationRecord
   end
 
   def user_or_network
-    errors.add(:base, "Necessario fornire utente o network") if self.network.blank? && self.user_id == nil
+    errors.add(:base, "Necessario fornire utente o network") if self.network.blank? && self.user_id.nil?
   end
 
   # def reload_authlevels_cache
