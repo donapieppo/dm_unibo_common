da60cb59c2eb19629058f5404b48b4c42bd38a3a
Pietro <donapieppo@yahoo.it>
2025-12-16 21:23:40 +0100

    routes

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  mount DmUniboCommon::Engine => "/dm_unibo_common"
+  mount DmUniboCommon::Engine => "/dm_unibo_common", :as => "dm_unibo_common"
+
+  get "/logins/logout", to: "dm_unibo_common/logins#logout"
+
   get "/show_if_current_organization", to: "home#show_if_current_organization"
+  get "/home", to: "home#index", as: "home"
   root to: "home#index"
 end
