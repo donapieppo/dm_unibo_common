6da7b44752da67a67c692dc1adcc9f3302548a44
Pietro <donapieppo@yahoo.it>
2025-12-22 10:22:42 +0100

    login

--- app/components/dm_unibo_common/menu_component.rb
+++ app/components/dm_unibo_common/menu_component.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
+
 class DmUniboCommon::MenuComponent < ViewComponent::Base
   include DmUniboCommon::IconHelper
-  # include DmUniboCommon::MenuHelper
+  include DmUniboCommon::MenuHelper
 
   def initialize(
     sso_user_upn,
@@ -19,33 +20,6 @@ class DmUniboCommon::MenuComponent < ViewComponent::Base
       header_subtitle
     end
     @search_component = search_component
-  end
-
-  # DmUniboCommon::Engine.routes.url_helpers.send("auth_shibboleth_callback_path") => "/dm_unibo_common/auth/shibboleth/callback"
-  # Rails.application.routes.url_helpers.send('dm_unibo_common.auth_shibboleth_callback_path') -> undefined method `dm_unibo_common.auth_shibboleth_callback_path'
-  # dm_unibo_common.auth_shibboleth_callback_path.inspect -> "/seminari/dm_unibo_common/auth/shibboleth/callback"
-  # root_path -> seminari
-  def login_link
-    txt = dm_icon("sign-in", text: "Login")
-
-    case Rails.configuration.unibo_common.omniauth_provider
-    when :shibboleth
-      helpers.dm_unibo_common.auth_shibboleth_callback_path
-    when :entra_id
-      button_to txt, "/dm_unibo_common/auth/entra_id", form: {data: {turbo: false}}
-    when :google_oauth2
-      button_to txt, "/dm_unibo_common/auth/google_oauth2", form: {data: {turbo: false}}
-    when :developer
-      button_to txt, "/dm_unibo_common/auth/developer", form: {data: {turbo: false}}
-    when :test
-      button_to txt, helpers.dm_unibo_common.auth_test_callback_path, form: {data: {turbo: false}}
-    end
-  end
-
-  def logout_link
-    link_to helpers.dm_unibo_common.logout_path, data: {turbo: false} do
-      dm_icon("sign-out", text: "Logout")
-    end
   end
 
   def main_root_path
