cd58928884111e4483d71c8894b92715f1f76350
Pietro <donapieppo@yahoo.it>
2025-12-23 09:13:14 +0100

    text

--- app/helpers/dm_unibo_common/application_helper.rb
+++ app/helpers/dm_unibo_common/application_helper.rb
@@ -57,7 +57,7 @@ module DmUniboCommon::ApplicationHelper
   end
 
   def check_user_is_cesia
-    current_user.is_cesia? or raise DmUniboCommon::NotAuthorized, "Non sufficienti privilegi per seguire l'operazione"
+    current_user.is_cesia? or raise DmUniboCommon::NotAuthorized, "Non sufficienti privilegi per seguire l'operazione."
   end
 
   def auth_callback_path
