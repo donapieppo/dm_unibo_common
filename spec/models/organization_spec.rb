4dd8e27ae790087c75e347dc0e837c92d62efe18
Pietro <donapieppo@yahoo.it>
2026-06-01 10:21:29 +0200

    spec in code

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -6,4 +6,20 @@ RSpec.describe Organization, type: :model do
   it "has many permissions" do
     expect(subject).to respond_to(:permissions) 
   end
+
+  it "allows code with letters, numbers, dots, underscores and hyphens" do
+    organization = FactoryBot.build(:organization, code: "ABC_12.foo-bar")
+    expect(organization).to be_valid
+  end
+
+  it "does not allow code with spaces, path separators or punctuation at the edges" do
+    organization = FactoryBot.build(:organization, code: "ABC 12/foo")
+    expect(organization).not_to be_valid
+
+    organization.code = ".ABC12"
+    expect(organization).not_to be_valid
+
+    organization.code = "ABC12-"
+    expect(organization).not_to be_valid
+  end
 end
