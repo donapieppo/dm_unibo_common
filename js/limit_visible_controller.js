3276e836d41dcda36a4fed543a3d378b4573b822
Pietro <donapieppo@yahoo.it>
2026-08-14 12:17:28 +0200

    indentation

--- js/limit_visible_controller.js
+++ js/limit_visible_controller.js
@@ -1,40 +1,40 @@
 import { Controller } from "@hotwired/stimulus"
 
 class LimitVisibleController extends Controller {
-        static targets = ["item"]
-        static values = { 
-                substring: {type: Boolean, default: false},
-                display: {type: String, default: "block"},
+  static targets = ["item"]
+  static values = { 
+    substring: {type: Boolean, default: false},
+    display: {type: String, default: "block"},
+  }
+
+  connect() {
+    console.log(`dm_unibo_common limit visible connected on ${this.displayValue} with substring=${this.substringValue}`);
+  }
+
+  update(e) {
+    var input;
+    var check;
+
+    if (e.target.type == "checkbox") {
+      // For checkbox set true / false as data-text="true"
+      input = e.target.checked.toString();
+    } else {
+      input = e.target.value.trim().toLowerCase();
+    }
+
+    console.log(`limit visible updated with ${input}`)
+
+    this.itemTargets.forEach((o) => {
+      if ('text' in o.dataset) {
+        if (this.substringValue) {
+          check = o.dataset.text.toLowerCase().includes(input)
+        } else {
+          check = o.dataset.text.toLowerCase() == input
         }
-
-        connect() {
-                console.log(`dm_unibo_common limit visible connected on ${this.displayValue} with substring=${this.substringValue}`);
-        }
-
-        update(e) {
-                var input;
-                var check;
-
-                if (e.target.type == "checkbox") {
-                        // For checkbox set true / false as data-text="true"
-                        input = e.target.checked.toString();
-                } else {
-                        input = e.target.value.trim().toLowerCase();
-                }
-                
-                console.log(`limit visible updated with ${input}`)
-
-                this.itemTargets.forEach((o) => {
-                        if ('text' in o.dataset) {
-                                if (this.substringValue) {
-                                        check = o.dataset.text.toLowerCase().includes(input)
-                                } else {
-                                        check = o.dataset.text.toLowerCase() == input
-                                }
-                                o.style.display = check ? this.displayValue : 'none';
-                        }
-                });
-        }
+        o.style.display = check ? this.displayValue : 'none';
+      }
+    });
+  }
 }
 
 export { LimitVisibleController as default };
