46d7bdc98104344ac2983025ba5224dcc3c5bc4e
Pietro <donapieppo@yahoo.it>
2026-09-11 12:58:55 +0200

    modal component refactor

--- app/components/dm_unibo_common/modal_component.rb
+++ app/components/dm_unibo_common/modal_component.rb
@@ -9,13 +9,37 @@
 class DmUniboCommon::ModalComponent < ViewComponent::Base
   include DmUniboCommon::ApplicationHelper
 
-  def initialize(title: "", hidden: false, noactions: false, size: :lg)
+  def initialize(title: "", noactions: false, size: :lg, modal_page: nil)
     @title = title
-    @hidden = hidden
     @size = size.to_s
+    @modal_page = modal_page
     @actions = noactions ? "" : "turbo:click->turbo-modal#followLink
                                  keyup@window->turbo-modal#closeWithKeyboard
                                  click@window->turbo-modal#closeBackground
                                  turbo:submit-end->turbo-modal#submitEnd"
+  end
+
+  def modal_page?
+    return @modal_page unless @modal_page.nil?
+
+    helpers.respond_to?(:modal_page?) ? helpers.modal_page? : false
+  end
+
+  def modal_attributes
+    attributes = {
+      id: "modal_div",
+      class: "modal fade",
+      tabindex: "-1",
+      role: "dialog",
+      aria: { modal: true }
+    }
+
+    if @title.present?
+      attributes[:aria][:labelledby] = "modal_title"
+    else
+      attributes[:aria][:label] = "Modal"
+    end
+
+    attributes
   end
 end
